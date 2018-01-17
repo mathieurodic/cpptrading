9fdc474dbd1513ca5bd7346764187d4163b903c4
Mathieu Rodic <mathieu@rodic.fr>
2018-01-17 23:52:55 +0100

    [TESTING] made tests more consistent for Gzip

--- tests/gzip.cpp
+++ tests/gzip.cpp
@@ -8,7 +8,7 @@ int main(int argc, char const *argv[]) {
     const std::string path = "db/test-gziplog.gz";
     std::remove(path.c_str());
     {
-        std::cout << "STORE" << '\n';
+        std::cout << "STORE...\n";
         size_t size = 0;
         GzipLogWriter writer(path);
         for (size_t i = 100000; i < 1000000; i++) {
@@ -18,19 +18,40 @@ int main(int argc, char const *argv[]) {
         }
         std::cout << "STORED " << size << " BYTES\n";
     }
-    {
-        std::cout << "READ" << '\n';
-        typedef char char4_t[6];
+    for (int _=0;_<4;++_) {
+        std::cout << "CHECK...\n";
+        typedef char char6_t[6];
+        size_t size = 0;
         GzipLogReader reader(path);
         for (size_t i = 100000; i < 1000000; i++) {
-            char4_t item_data;
-            reader.next<char4_t>(item_data);
+            size += sizeof(char6_t);
+            char6_t item_data;
+            auto result = reader.next<char6_t>(item_data);
             const std::string item((const char*) &item_data, sizeof(item_data));
             const std::string expected_item = std::to_string(i);
             if (item != expected_item) {
                 std::cout << "GOT " << item << ", EXPECTED " << expected_item << '\n';
             }
         }
+        std::cout << "CHECKED " << size << " BYTES\n";
+    }
+    for (int _=0;_<4;++_) {
+        std::cout << "FINISH..." << '\n';
+        typedef char char6_t[6];
+        GzipLogReader reader(path);
+        char6_t item_data;
+        size_t size = 0;
+        size_t i = 100000;
+        while (reader.next<char6_t>(item_data) == sizeof(char6_t)) {
+            size += sizeof(char6_t);
+            const std::string item((const char*) &item_data, sizeof(item_data));
+            const std::string expected_item = std::to_string(i);
+            if (item != expected_item) {
+                std::cout << "GOT " << item << ", EXPECTED " << expected_item << '\n';
+            }
+            ++i;
+        }
+        std::cout << "FINISHED, READ " << size << " BYTES\n";
     }
     return 0;
 }
