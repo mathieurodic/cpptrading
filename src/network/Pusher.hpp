ae0d641111a1ee893698b1b8e2badad591ea5dde
Mathieu Rodic <mathieu@rodic.fr>
2018-01-22 23:42:02 +0100

    [FEATURE] PusherConnection is now threaded (for asynchronous capabilities)

--- src/network/Pusher.hpp
+++ src/network/Pusher.hpp
@@ -5,6 +5,8 @@
 #include <string>
 #include <vector>
 #include <map>
+#include <set>
+#include <thread>
 
 #include "./easywsclient.hpp"
 #include "./easywsclient.cpp"
@@ -18,27 +20,33 @@ using JSON = nlohmann::json;
 typedef void (*pusher_callback_t) (const std::string&, const JSON&, void*);
 
 
+struct PusherChannelSubscription {
+    inline PusherChannelSubscription(const std::string& _channel_name, const pusher_callback_t _callback, void* _user_data) :
+        channel_name(_channel_name),
+        callback(_callback),
+        user_data(_user_data) {}
+    const std::string channel_name;
+    const pusher_callback_t callback;
+    void* user_data;
+};
+
+
 class PusherConnection {
 public:
 
     inline PusherConnection(const std::string& key, const std::string& cluster="mt1") :
         _key(key),
+        _cluster(cluster),
         _host("ws-" + cluster + ".pusher.com"),
         _path("/app/" + key + "?client=cpptrading&version=0.01&protocol=7"),
         _url("ws://" + _host + _path),
-        _ws(easywsclient::WebSocket::from_url(_url))
-    {
-        if (_ws == NULL) {
-            throw NetworkException("Could not connect to Pusher", _key, cluster);
-        }
-    }
+        _ws(NULL),
+        _thread(NULL),
+        _is_running(false)
+    {}
 
     inline ~PusherConnection() {
-        if (_ws) {
-            _ws->close();
-            delete _ws;
-            _ws = NULL;
-        }
+        stop();
     }
 
     inline void send(const std::string& event, const JSON& data) {
@@ -49,16 +57,49 @@ public:
         _ws->send(payload);
     }
     inline void subscribe(const std::string& channel_name, std::vector<std::string> events, pusher_callback_t callable, void* user_data=NULL) {
-        send("pusher:subscribe", {{"channel", channel_name}});
         for (const std::string& event : events) {
-            _callbacks.insert({event, {callable, user_data}});
+            _subscriptions.insert({event, {channel_name, callable, user_data}});
         }
     }
 
+    static void _start(PusherConnection* pusher_connection) {
+        while (pusher_connection->_is_running) {
+            pusher_connection->_ws->poll(1);
+            pusher_connection->_ws->dispatch(pusher_connection->_callback, pusher_connection);
+        }
+    }
     inline void start() {
-        while (_ws) {
-            _ws->poll(1);
-            _ws->dispatch(_callback, this);
+        stop();
+        _is_running = true;
+        // initialize websocket
+        _ws = easywsclient::WebSocket::from_url(_url);
+        if (_ws == NULL) {
+            throw NetworkException("Could not connect to Pusher WebSocket", _key, _cluster);
+        }
+        // initialize websocket channels
+        std::set<std::string> channel_names;
+        for (const auto& it : _subscriptions) {
+            channel_names.insert(it.second.channel_name);
+        }
+        for (const std::string& channel_name : channel_names) {
+            send("pusher:subscribe", {{"channel", channel_name}});
+        }
+        // initialize thread
+        _thread = new std::thread(_start, this);
+    }
+    inline void stop() {
+        _is_running = false;
+        // stop thread
+        if (_thread) {
+            _thread->join();
+            delete _thread;
+            _thread = NULL;
+        }
+        // stop websocket
+        if (_ws) {
+            _ws->close();
+            delete _ws;
+            _ws = NULL;
         }
     }
 
@@ -67,12 +108,13 @@ public:
         try {
             const std::string event = message["event"];
             const auto data = JSON::parse(message["data"].get<std::string>());
-            auto range = _callbacks.equal_range(event);
+            auto range = _subscriptions.equal_range(event);
             for (auto it=range.first; it!=range.second; ++it) {
-                it->second.first(event, data, it->second.second);
+                const PusherChannelSubscription& subsciption = it->second;
+                subsciption.callback(event, data, subsciption.user_data);
             }
         } catch (const nlohmann::detail::type_error&) {
-            std::cout << "UNEXPECTED PAYLOAD FORMAT" << std::endl;
+            std::cout << "UNEXPECTED PAYLOAD FORMAT: " << payload << std::endl;
         }
     }
     static void _callback(const std::string& payload, void* _pusher_connection) {
@@ -81,12 +123,15 @@ public:
     }
 
 private:
+    bool _is_running;
     const std::string _key;
+    const std::string _cluster;
     const std::string _host;
     const std::string _path;
     const std::string _url;
     easywsclient::WebSocket::pointer _ws;
-    std::multimap<std::string, std::pair<pusher_callback_t, void*>> _callbacks;
+    std::thread* _thread;
+    std::multimap<std::string, PusherChannelSubscription> _subscriptions;
 };
 
 
--- tests/pusher.cpp
+++ tests/pusher.cpp
@@ -1,6 +1,6 @@
-#include "network/Pusher.hpp"
-
 #include <iostream>
+
+#include "network/Pusher.hpp"
 
 
 void trades_callback(const std::string& event, const JSON& data, void* user_data) {
@@ -14,11 +14,17 @@ void orders_callback(const std::string& event, const JSON& data, void* user_data
 }
 
 
+#include <thread>
+#include <chrono>
+
 int main(int argc, char const *argv[]) {
     PusherConnection connection("de504dc5763aeef9ff52");
     connection.subscribe("live_trades_btceur", {"trade"}, trades_callback);
     connection.subscribe("live_trades_btceur", {"trade"}, trades_callback_2);
     connection.subscribe("live_orders_btceur", {"order_created", "order_changed", "order_deleted"}, orders_callback);
     connection.start();
+    std::this_thread::sleep_for(
+        std::chrono::seconds(15)
+    );
     return 0;
 }
