37437e941d28f4c1d93a9ab3aec25f6ed66e4371
Mathieu Rodic <mathieu@rodic.fr>
2018-01-28 23:17:22 +0100

    [DEBUG] an exception is now thrown in case make_directory fails

--- src/IO/directories.hpp
+++ src/IO/directories.hpp
@@ -12,7 +12,7 @@ const inline void make_directory(std::experimental::filesystem::path path) {
     path = std::experimental::filesystem::absolute(path);
     if (!std::experimental::filesystem::is_directory(path)) {
         if (std::experimental::filesystem::exists(path)) {
-            FileException("Could not create directory `" + path.string() + "`, path exists as something else than a directory");
+            throw FileException("Could not create directory `" + path.string() + "`, path exists as something else than a directory");
         }
         std::experimental::filesystem::path parent_path = path.parent_path();
         if (path.string().size()) {
