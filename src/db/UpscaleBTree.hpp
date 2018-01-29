d732be25217aca3b93de8d3cf472e586bcc3a43f
Mathieu Rodic <mathieu@rodic.fr>
2018-01-29 21:23:33 +0100

    [FEATURE] transaction are everywhere for reading

--- src/db/UpscaleBTree.hpp
+++ src/db/UpscaleBTree.hpp
@@ -39,8 +39,9 @@ public:
         memcpy(this, &source, sizeof(*this));
     }
     inline UpscaleBTreeCursor() : _is_finished(true) {}
-    inline UpscaleBTreeCursor(ups_db_t* ups_db, bool is_fullrange, key_t key_begin, key_t key_end) :
+    inline UpscaleBTreeCursor(ups_db_t* ups_db, ups_txn_t* ups_txn, bool is_fullrange, key_t key_begin, key_t key_end) :
         _ups_db(ups_db),
+        _ups_txn(ups_txn),
         _ups_cursor(NULL),
         _is_fullrange(is_fullrange),
         _is_reverse(is_fullrange ? false : (key_end < key_begin)),
@@ -56,6 +57,7 @@ public:
         if (_ups_cursor) {
             UPS_SAFE_CALL(ups_cursor_close,
                 _ups_cursor);
+            _ups_cursor = NULL;
         }
     }
 
@@ -159,6 +161,7 @@ private:
     //
     key_t _key_begin, _key_end;
     ups_db_t* _ups_db;
+    ups_txn_t* _ups_txn;
     ups_cursor_t* _ups_cursor;
 };
 
@@ -166,12 +169,13 @@ private:
 template <typename key_t, typename record_t>
 class UpscaleBTreeRange {
 public:
-    UpscaleBTreeRange(ups_db_t* ups_db) :
+    UpscaleBTreeRange(ups_db_t* ups_db, ups_txn_t* ups_txn) :
         _ups_db(ups_db),
+        _ups_txn(ups_txn),
         _is_fullrange(true)
     {}
-    UpscaleBTreeRange(ups_db_t* ups_db, key_t key_begin, key_t key_end) :
-        UpscaleBTreeRange(ups_db)
+    UpscaleBTreeRange(ups_db_t* ups_db, ups_txn_t* ups_txn, key_t key_begin, key_t key_end) :
+        UpscaleBTreeRange(ups_db, ups_txn)
     {
         memcpy(&_key_begin, &key_begin, sizeof(key_t));
         memcpy(&_key_end, &key_end, sizeof(key_t));
@@ -179,7 +183,7 @@ public:
     }
 
     UpscaleBTreeCursor<key_t, record_t> begin() {
-        return UpscaleBTreeCursor<key_t, record_t>(_ups_db, _is_fullrange, _key_begin, _key_end);
+        return UpscaleBTreeCursor<key_t, record_t>(_ups_db, _ups_txn, _is_fullrange, _key_begin, _key_end);
     }
 
     const UpscaleBTreeCursor<key_t, record_t>& end() {
@@ -191,6 +195,7 @@ private:
     bool _is_fullrange;
     key_t _key_begin, _key_end;
     ups_db_t* _ups_db;
+    ups_txn_t* _ups_txn;
     //
     static const UpscaleBTreeCursor<key_t, record_t> _cursor_end;
 };
@@ -326,7 +331,7 @@ public:
         try {
             UPS_SAFE_CALL(ups_db_find,
                 _ups_db,
-                NULL, // transaction
+                _ups_read_txn,
                 &ups_key,
                 &ups_record,
                 flags
@@ -342,10 +347,10 @@ public:
     }
 
     inline UpscaleBTreeRange<key_t, record_t> get_all() {
-        return UpscaleBTreeRange<key_t, record_t>(_ups_db);
+        return UpscaleBTreeRange<key_t, record_t>(_ups_db, _ups_read_txn);
     }
     inline UpscaleBTreeRange<key_t, record_t> get_range(key_t key_begin, key_t key_end) {
-        return UpscaleBTreeRange<key_t, record_t>(_ups_db, key_begin, key_end);
+        return UpscaleBTreeRange<key_t, record_t>(_ups_db, _ups_read_txn, key_begin, key_end);
     }
 
     inline const bool contains(record_t& searched_record) {
@@ -375,7 +380,7 @@ public:
         UPS_SAFE_CALL(ups_cursor_create,
             &ups_cursor,
             _ups_db,
-            0, // transaction
+            _ups_read_txn,
             0 // flags (unused)
         );
         try {
@@ -386,20 +391,25 @@ public:
                 UPS_FIND_GEQ_MATCH
             );
         } catch (const UpscaleDBException& exception) {
+            UPS_SAFE_CALL(ups_cursor_close,
+                ups_cursor
+            );
             if (exception.get_status() == UPS_KEY_NOT_FOUND || exception.get_status() == UPS_INV_PARAMETER || exception.get_status() == UPS_CURSOR_IS_NIL) {
                 return 0;
+            } else {
+                throw exception;
             }
         }
         // test until record is found... or not
         size_t count = 0;
         do {
             if (key > searched_key) {
-                return count;
+                break;
             }
             if (record == searched_record) {
                 ++count;
                 if (stop_at_first) {
-                    return count;
+                    break;
                 }
             }
             try {
@@ -411,11 +421,18 @@ public:
                 );
             } catch (const UpscaleDBException& exception) {
                 if (exception.get_status() == UPS_KEY_NOT_FOUND || exception.get_status() == UPS_INV_PARAMETER) {
-                    return count;
+                    break;
                 }
+                UPS_SAFE_CALL(ups_cursor_close,
+                    ups_cursor
+                );
                 throw exception;
             }
         } while (true);
+        UPS_SAFE_CALL(ups_cursor_close,
+            ups_cursor
+        );
+        return count;
     }
     inline const bool contains(key_t& searched_key) {
         return count(searched_key, true);
@@ -441,7 +458,7 @@ public:
         UPS_SAFE_CALL(ups_cursor_create,
             &ups_cursor,
             _ups_db,
-            0, // transaction
+            _ups_read_txn,
             0 // flags (unused)
         );
         try {
@@ -452,20 +469,25 @@ public:
                 UPS_FIND_GEQ_MATCH
             );
         } catch (const UpscaleDBException& exception) {
+            UPS_SAFE_CALL(ups_cursor_close,
+                ups_cursor
+            );
             if (exception.get_status() == UPS_KEY_NOT_FOUND || exception.get_status() == UPS_INV_PARAMETER || exception.get_status() == UPS_CURSOR_IS_NIL) {
                 return 0;
+            } else {
+                throw exception;
             }
         }
         // test until record is found... or not
         size_t count = 0;
         do {
             if (key > searched_key) {
-                return count;
+                break;
             }
             if (key == searched_key) {
                 ++count;
                 if (stop_at_first) {
-                    return count;
+                    break;
                 }
             }
             try {
@@ -477,11 +499,18 @@ public:
                 );
             } catch (const UpscaleDBException& exception) {
                 if (exception.get_status() == UPS_KEY_NOT_FOUND || exception.get_status() == UPS_INV_PARAMETER) {
-                    return count;
+                    break;
                 }
+                UPS_SAFE_CALL(ups_cursor_close,
+                    ups_cursor
+                );
                 throw exception;
             }
         } while (true);
+        UPS_SAFE_CALL(ups_cursor_close,
+            ups_cursor
+        );
+        return count;
     }
 
 protected:
