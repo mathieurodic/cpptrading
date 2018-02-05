d013a4f069b41b586fd2a9b7816f8a37c844d1a2
Mathieu Rodic <mathieu@rodic.fr>
2018-02-05 21:18:07 +0100

    [FEATURE] introduced the concepts of ranges & iterators for history

--- src/history/History.hpp
+++ src/history/History.hpp
@@ -3,7 +3,51 @@
 
 
 #include <models/Trade.hpp>
+#include <models/TradeAverage.hpp>
 #include <models/Order.hpp>
+#include <models/Decision.hpp>
+
+
+template <typename Model>
+class HistoryIterator {
+public:
+
+    HistoryIterator() : _is_finished(true) {}
+
+    virtual const Model& operator*() const {
+        return _value;
+    }
+
+    virtual void operator++() {}
+
+    virtual const bool operator!=(const HistoryIterator<Model>& other) const {
+        return _is_finished != other._is_finished;
+    }
+
+private:
+
+    bool _is_finished;
+    Model _value;
+
+};
+
+
+template <typename Model>
+class HistoryRange {
+public:
+
+    HistoryRange() {}
+
+    virtual HistoryIterator<Model> begin() {
+        return HistoryIterator<Model>();
+    }
+    const HistoryIterator<Model>& end() const {
+        return _end;
+    }
+
+private:
+    static const HistoryIterator<Model> _end;
+};
 
 
 class History {
@@ -11,6 +55,15 @@ public:
 
     virtual void feed(Trade& trade) = 0;
     virtual void feed(Order& order) = 0;
+    virtual void feed(Decision& decision) = 0;
+
+    virtual TradeAverage get_average(const double& timestamp_begin, const double timestamp_end) {
+        return TradeAverage();
+    }
+
+    virtual HistoryRange<Trade> get_trades() {
+        return HistoryRange<Trade>();
+    }
 
 };
 
