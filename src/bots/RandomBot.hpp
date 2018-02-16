49aacf594c062062e6a950c49bd8a506c1ddd7b0
Mathieu Rodic <mathieu@rodic.fr>
2018-02-16 21:07:56 +0100

    [FEATURE] RandomBot is now more customizable

--- src/bots/RandomBot.hpp
+++ src/bots/RandomBot.hpp
@@ -10,26 +10,33 @@
 class RandomBot : public Bot {
 public:
 
-    inline RandomBot(History& history, Broker& broker) :
+    inline RandomBot(History& history, Broker& broker, const double& probabity=.1) :
         Bot(history, broker),
+        _probabity(probabity),
         _price(10000.) {}
+
+    virtual std::string get_name() const {
+        return "RandomBot";
+    }
 
     virtual Decision decide(const Timestamp& timestamp) {
         _price += (rand() / (double) RAND_MAX) - .5;
         Decision decision;
-        decision.type = (ActionType) (rand() % 3 - 1);
-        if (rand() % 2) {
+        if (rand() > _probabity * (double) RAND_MAX) {
             decision.type = WAIT;
+        } else {
+            decision.confidence = rand() / (double) RAND_MAX;
+            decision.decision_timestamp = timestamp;
+            decision.price = _price;
+            decision.type = (rand() % 2) ? BUY : SELL;
         }
-        decision.confidence = rand() / (double) RAND_MAX;
-        decision.decision_timestamp = timestamp;
-        decision.price = _price;
         return decision;
     }
 
 private:
 
     double _price;
+    const double _probabity;
 
 };
 
